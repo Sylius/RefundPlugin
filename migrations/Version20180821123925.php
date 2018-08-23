5e3bc27c6f7b7779e251ea7d688d30d045567cd3
Mateusz Zalewski <zaleslaw@gmail.com>
2018-08-23 10:37:46 +0200

    Default value in migration

--- migrations/Version20180821123925.php
+++ migrations/Version20180821123925.php
@@ -14,7 +14,7 @@ final class Version20180821123925 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE sylius_refund_credit_memo ADD comment VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE sylius_refund_credit_memo ADD comment VARCHAR(255) NOT NULL DEFAULT ""');
     }
 
     public function down(Schema $schema) : void
