ca3612ca994eef0ee89571c41c8e3bb9d83bf444
Mateusz Zalewski <zaleslaw@gmail.com>
2018-10-31 11:15:24 +0100

    Support only Symfony 4.1 version

--- src/Action/Admin/RefundUnitsAction.php
+++ src/Action/Admin/RefundUnitsAction.php
@@ -11,7 +11,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
-use Webmozart\Assert\Assert;
 
 final class RefundUnitsAction
 {
