0360649eba9ac4140ab50f9ff10e07da7923238a
Adam Kasperczak <adamkasperczak123@gmail.com>
2019-05-15 16:07:47 +0200

    Modified RefundType and used php-enum library

--- src/Model/RefundType.php
+++ src/Model/RefundType.php
@@ -4,42 +4,20 @@ declare(strict_types=1);
 
 namespace Sylius\RefundPlugin\Model;
 
-use Sylius\RefundPlugin\Exception\RefundTypeNotResolved;
+use MyCLabs\Enum\Enum;
 
-final class RefundType
+class RefundType extends Enum
 {
     public const ORDER_ITEM_UNIT = 'order_item_unit';
     public const SHIPMENT = 'shipment';
 
-    /** @var string */
-    private $value;
-
-    public function __construct(string $value)
-    {
-        if (!in_array($value, [self::ORDER_ITEM_UNIT, self::SHIPMENT])) {
-            throw RefundTypeNotResolved::withType($value);
-        }
-
-        $this->value = $value;
-    }
-
     public static function orderItemUnit(): self
     {
-        return new self(self::ORDER_ITEM_UNIT);
+        return new RefundType(self::ORDER_ITEM_UNIT);
     }
 
     public static function shipment(): self
     {
-        return new self(self::SHIPMENT);
-    }
-
-    public function equals(self $refundType): bool
-    {
-        return $this->__toString() === $refundType->__toString();
-    }
-
-    public function __toString(): string
-    {
-        return $this->value;
+        return new RefundType(self::SHIPMENT);
     }
 }
