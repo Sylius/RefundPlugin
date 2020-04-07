ab7667afe6d88ab0770198915ec99519e2baceaf
Kévin Dunglas <dunglas@gmail.com>
2020-04-07 11:40:22 +0200

    Migrations: fix compatibility with MySQL 8

--- migrations/Version20190517064223.php
+++ migrations/Version20190517064223.php
@@ -17,7 +17,7 @@ final class Version20190517064223 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE sylius_refund_refund CHANGE type type VARCHAR(256) COMMENT "sylius_refund_refund_type" NOT NULL COMMENT \'(DC2Type:sylius_refund_refund_type)\'');
+        $this->addSql('ALTER TABLE sylius_refund_refund CHANGE type type VARCHAR(256) NOT NULL COMMENT \'(DC2Type:sylius_refund_refund_type)\'');
     }
 
     public function down(Schema $schema): void
