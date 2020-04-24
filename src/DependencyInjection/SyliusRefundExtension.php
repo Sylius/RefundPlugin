2b9724469f103982d731a7f946970bdd1752a47a
Kamil Kokot <kamil@kokot.me>
2020-04-24 14:22:03 +0200

    Set up Doctrine Migrations v3.0

--- src/DependencyInjection/SyliusRefundExtension.php
+++ src/DependencyInjection/SyliusRefundExtension.php
@@ -7,9 +7,10 @@ namespace Sylius\RefundPlugin\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
-final class SyliusRefundExtension extends Extension
+final class SyliusRefundExtension extends Extension implements PrependExtensionInterface
 {
     /**
      * {@inheritdoc}
@@ -20,5 +21,24 @@ final class SyliusRefundExtension extends Extension
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
 
         $loader->load('services.xml');
+    }
+
+    public function prepend(ContainerBuilder $container): void
+    {
+        if (!$container->hasExtension('doctrine_migrations') || !$container->hasExtension('sylius_core')) {
+            return;
+        }
+
+        $container->prependExtensionConfig('doctrine_migrations', [
+            'migrations_paths' => [
+                'Sylius\RefundPlugin\Migrations' => __DIR__. '/../Migrations',
+            ],
+        ]);
+
+        $container->prependExtensionConfig('sylius_core', [
+            'migrations' => [
+                'Sylius\RefundPlugin\Migrations' => ['Sylius\Bundle\CoreBundle\Migrations'],
+            ],
+        ]);
     }
 }
