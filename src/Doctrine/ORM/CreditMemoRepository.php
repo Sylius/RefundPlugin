1f96d2ecec313a2224c5b4fd21c6413de1547321
Clemence <clemence.pereira@hotmail.fr>
2020-12-30 12:55:26 +0100

    Create CreditMemo repository to find order by Id and order them correctly

---
+++ src/Doctrine/ORM/CreditMemoRepository.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\RefundPlugin\Doctrine\ORM;
+
+use Sylius\Bundle\ResourceBundle\Doctrine\ORM\EntityRepository;
+use Sylius\RefundPlugin\Entity\CreditMemoInterface;
+use Sylius\RefundPlugin\Repository\CreditMemoRepositoryInterface;
+
+class CreditMemoRepository extends EntityRepository implements CreditMemoRepositoryInterface
+{
+    public function findByOrderId($orderId)
+    {
+        return $this->createQueryBuilder('c')
+            ->andWhere('c.order = :orderId')
+            ->addOrderBy('c.issuedAt', 'ASC')
+            ->setParameter('orderId', $orderId)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+}
