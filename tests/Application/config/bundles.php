0c9e9d8e95e81a6d68fd29eecee7e415b6a8402d
Tomanhez <tomek.sut@gmail.com>
2021-02-08 13:10:48 +0100

    Add next iteration

--- tests/Application/config/bundles.php
+++ tests/Application/config/bundles.php
@@ -57,5 +57,4 @@ return [
     Sylius\Bundle\ApiBundle\SyliusApiBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     SyliusLabs\DoctrineMigrationsExtraBundle\SyliusLabsDoctrineMigrationsExtraBundle::class => ['all' => true],
-    BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
 ];
