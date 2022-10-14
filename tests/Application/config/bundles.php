f12db7f5294bf0da5e5f34cc5027f104c2f5d4af
Kevin Kaniaburka <kevin.kaniaburka@gmail.com>
2022-10-14 11:50:48 +0200

    [Maintenance] Drop support for Sylius 1.10 and Symfony 4.4

--- tests/Application/config/bundles.php
+++ tests/Application/config/bundles.php
@@ -56,4 +56,5 @@ return [
     SyliusLabs\DoctrineMigrationsExtraBundle\SyliusLabsDoctrineMigrationsExtraBundle::class => ['all' => true],
     BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
     SyliusLabs\Polyfill\Symfony\Security\Bundle\SyliusLabsPolyfillSymfonySecurityBundle::class => ['all' => true],
+    Sylius\Calendar\SyliusCalendarBundle::class => ['all' => true],
 ];
